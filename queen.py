476b06fe56ebf98a44bb98c82bcedd671640cae3
billgoswell <billsgoswell@gmail.com>
2025-09-30 14:11:56 -0400

    Queen Movement

--- queen.py
+++ queen.py
@@ -6,3 +6,7 @@ class Queen(Piece):
         super().__init__(color, x, y, size)
         self.image = self.get_img(size, 1)
 
+    def possible_moves(self, board):
+        moves = self.diag_moves(board)
+        moves.extend(self.horz_moves(board))
+        return moves
